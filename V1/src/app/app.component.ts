66ed10f5392737ea6cbfa51829a21bc470d1ab2e
Shamika240 <wijesingheshamika@gmail.com>
2020-07-03 12:17:35 +0530

    Created the branch

--- V1/src/app/app.component.ts
+++ V1/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'V1';
+
+  signOut() {
+    localStorage.removeItem('currentUser');
+  }
 }
--- V1/src/app/app.module.ts
+++ V1/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnalyzerSelectionComponent } from './analyzer-selection/analyzer-selection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnalyzerSelectionComponent
   ],
   imports: [
     BrowserModule,
