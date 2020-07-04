c1b4c1aca92ea22a46e30bf323fb001716db4523
Kansaji <lakruwan45@gmail.com>
2020-07-04 23:26:34 +0530

    Group8-88

---
+++ V1/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {NavbarComponent} from './navbar/navbar.component'
+@Component({
+  selector: 'app-admin-dashboard',
+  templateUrl: './admin-dashboard.component.html',
+  styleUrls: ['./admin-dashboard.component.css']
+})
+export class AdminDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- V1/src/app/app.module.ts
+++ V1/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { NavbarComponent } from './admin-dashboard/navbar/navbar.component';
+import { UsersComponent } from './admin-dashboard/users/users.component';
+import { NewuserComponent } from './admin-dashboard/newuser/newuser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminDashboardComponent,
+    NavbarComponent,
+    UsersComponent,
+    NewuserComponent
   ],
   imports: [
     BrowserModule,
