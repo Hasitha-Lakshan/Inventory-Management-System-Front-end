bce082a655e57b9935b82af3758396df056b9eb7
Kavinda <kavindanimalaka97@gmail.com>
2020-07-06 03:23:44 +0530

    Merge Conflict

--- V1/src/app/app.module.ts
+++ V1/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReportComponent } from './report/report.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
