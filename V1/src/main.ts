f3a2b5e2cacb7c07aee2e38c6934a48da44ea9f0
Kavinda <kavindanimalaka97@gmail.com>
2020-07-06 07:55:31 +0530

    Included the search bar and Implemented Navigation bar

--- V1/src/main.ts
+++ V1/src/main.ts
@@ -4,6 +4,44 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import './polyfills';
+
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DemoMaterialModule} from './app/material-module';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+
+import {DatepickerCustomIconExample} from './app/datepicker-custom-icon-example';
+ 
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+  ],
+  entryComponents: [DatepickerCustomIconExample],
+  declarations: [DatepickerCustomIconExample],
+  bootstrap: [DatepickerCustomIconExample],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+  ]
+})
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+
+
 if (environment.production) {
   enableProdMode();
 }
