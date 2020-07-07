769457e1878c10b304df15a2589b10d2b16b5fb2
Kavinda <kavindanimalaka97@gmail.com>
2020-07-07 12:15:25 +0530

    added the search button and updated time range

--- V1/src/app/app.module.ts
+++ V1/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/*import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; */
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReportsComponent } from './reports/reports.component';
@@ -11,6 +14,7 @@ import { ReportsComponent } from './reports/reports.component';
     ReportsComponent
   ],
   imports: [
+    /* NgbModule, */
     BrowserModule,
     AppRoutingModule
   ],
--- V1/src/app/reports/reports.component.ts
+++ V1/src/app/reports/reports.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbDate, NgbCalendar, NgbDateParserFormatter} from '@ng-bootstrap/ng-bootstrap'; 
+ 
 
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
   styleUrls: ['./reports.component.css']
 })
+
+
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor() { } 
 
   ngOnInit(): void {
-  }
+  } 
+} 
 
-}
+ 
