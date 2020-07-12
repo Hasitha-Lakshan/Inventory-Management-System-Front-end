ed661617c887c095db82abc647f8ebc4e6d3f049
supunika <supunikadharmakeerthi@gmail.com>
2020-07-12 18:31:54 +0100

    Inventory manager dashboard part 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NavbarComponent} from './navbar/navbar.component'
+import {ContainerComponent} from './container/container.component'
+import {NewproductsComponent} from './newproducts/newproducts.component'
+import { from } from 'rxjs';
+import { ReportsComponent } from './reports/reports.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'navbar',
+    component:NavbarComponent
+  },
+  {
+    path:'container',
+    component:ContainerComponent
+  },
+  {
+    path:'newproduct',
+    component:NewproductsComponent
+  },
+  {
+    path:'reports',
+    component:ReportsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NewproductsComponent } from './newproducts/newproducts.component';
+import { ReportsComponent } from './reports/reports.component';
+import { ContainerComponent } from './container/container.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NewproductsComponent,
+    ReportsComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
